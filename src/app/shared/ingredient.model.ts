d11ef0279d30c999c252bf7c271368106cd3cfb5
subhashini <subhashini.parthasarathy@dnata.com>
2017-10-15 12:35:01 +0400

    shopping code initial commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient {
+   /*  public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number){
+        this.name = name;
+        this.amount = amount;
+    } */
+
+    /* Typescript supports the following structure which
+    eventually converted into the code written above */
+    constructor(public name: string, public amount: number){        
+    }
+
+}
