5edc7c0c1e208d9195f3db0d127dcd91ecfd1aea
subhashini <subha.jobss@gmail.com>
2017-10-17 12:09:21 +0400

    Event Emitter implementation between Recipe and Shopping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  featureSelected = 'recipe'
+
+  navigateFeature(feature: string){
+    this.featureSelected = feature;
+  }
+
 }
